cf8c461433f8ca0bbcf072e4f28cde35953e1ffb
DoctorWu0 <36178894+DoctorWu0@users.noreply.github.com>
2018-03-07 15:36:41 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','root');
+define('DB_NAME','message');
+
+$db = @mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+if (!$db){
+    echo '数据库连接错误';
+}
